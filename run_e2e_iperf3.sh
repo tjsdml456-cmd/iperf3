333609c155412817dd58df608c2749b2eb9d2dc9
tjsdml456-cmd <tjsdml456@gmail.com>
2026-01-07 19:33:00 +0900

    Update run_e2e_iperf3.sh

--- run_e2e_iperf3.sh
+++ run_e2e_iperf3.sh
@@ -24,8 +24,8 @@ echo "=== 3단계: 외부 서버에서 UE로 DSCP 설정된 DL 트래픽 전송 
 iperf3 -c 10.45.0.2 -t 30 -p 6500 -i 1 -S 0x00 &
 # 5qi 3 (dscp 32)
 iperf3 -c 10.45.0.3 -t 30 -p 6501 -i 1 -S 0x80 &
-# 5qi 5 (dscp 40)
-iperf3 -c 10.45.0.4 -t 30 -p 6502 -i 1 -S 0xA0 &
+# 5qi 85 (dscp 14)
+iperf3 -c 10.45.0.4 -t 30 -p 6502 -i 1 -S 0x38 &
 
 sleep 2
 
@@ -34,8 +34,8 @@ echo "=== 4단계: UE에서 외부 서버로 UL 트래픽 전송 ==="
 sudo ip netns exec ue1 iperf3 -c ${EXTERNAL_SERVER_IP} -t 30 -p 6600 -i 1 -S 0x00 &
 # 5qi 3 (dscp 32) - UL
 sudo ip netns exec ue2 iperf3 -c ${EXTERNAL_SERVER_IP} -t 30 -p 6601 -i 1 -S 0x80 &
-# 5qi 5 (dscp 40) - UL
-sudo ip netns exec ue3 iperf3 -c ${EXTERNAL_SERVER_IP} -t 30 -p 6602 -i 1 -S 0xA0 &
+# 5qi 85 (dscp 14) - UL
+sudo ip netns exec ue3 iperf3 -c ${EXTERNAL_SERVER_IP} -t 30 -p 6602 -i 1 -S 0x38 &
 
 echo ""
 echo "트래픽 전송 중... 다른 터미널에서 로그 확인:"
